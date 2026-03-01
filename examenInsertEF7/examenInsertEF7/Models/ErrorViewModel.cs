7f0f5d3bd7c2dbca5acf145b92aafd3329d49f92
Tomas Cienega <tomascienega0206@gmail.com>
2026-02-28 18:31:30 -0600

    examenInsertEF7

---
+++ examenInsertEF7/examenInsertEF7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace examenInsertEF7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ examenInsertEF7/examenInsertEF7/ViewModel/EmpleadoVM.cs
@@ -0,0 +1,11 @@
+﻿using examenInsertEF7.Models;
+
+namespace examenInsertEF7.ViewModel
+{
+    public class EmpleadoVM
+    {
+        public List<Empleado> ListaEmpleados { get; set; } = new();
+        public Empleado EmpleadoModelReference { get; set; } = new();
+        public List<Departamento> ListaDepartamentos { get; set; } = new();
+    }
+}
